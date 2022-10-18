f1a965fb46529b2313d0b5311f3d1f11c0806b49
wtj <zhangwt1997@gmail.com>
2022-10-18 18:09:58 +0800

    handle the new option internally

--- compiler/rustc_codegen_ssa/src/back/write.rs
+++ compiler/rustc_codegen_ssa/src/back/write.rs
@@ -118,6 +118,9 @@ pub struct ModuleConfig {
     pub merge_functions: bool,
     pub emit_lifetime_markers: bool,
     pub llvm_plugins: Vec<String>,
+
+    // Inner unikernel
+    pub iu_playground: bool,
 }
 
 impl ModuleConfig {
@@ -266,6 +269,8 @@ impl ModuleConfig {
 
             emit_lifetime_markers: sess.emit_lifetime_markers(),
             llvm_plugins: if_regular!(sess.opts.unstable_opts.llvm_plugins.clone(), vec![]),
+
+            iu_playground: sess.opts.cg.iu_playground,
         }
     }
 
