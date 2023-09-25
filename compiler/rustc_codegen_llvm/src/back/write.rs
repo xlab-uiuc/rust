dce81a2aabaa1b0654b09629a102a58a123011f3
Jinghao Jia <jinghao7@illinois.edu>
2023-09-25 16:11:10 -0500

    disable llvm extra pass debug log
    Signed-off-by: Jinghao Jia <jinghao7@illinois.edu>

--- compiler/rustc_codegen_llvm/src/back/write.rs
+++ compiler/rustc_codegen_llvm/src/back/write.rs
@@ -772,8 +772,6 @@ pub(crate) unsafe fn llvm_optimize(
 
     let extra_passes = if !is_lto { passes_plus_iu.join(",") } else { "".to_string() };
 
-    llvm_note(diag_handler, &format!("extra_passes {}", extra_passes));
-
     let llvm_plugins = config.llvm_plugins.join(",");
 
     let enzyme_fn = if consider_ad {
