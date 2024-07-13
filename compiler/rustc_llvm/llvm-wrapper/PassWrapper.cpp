3c39072ba3dba18007a57ea7b6b7bad7d2e40ed3
Jinghao Jia <jinghao7@illinois.edu>
2024-07-13 02:36:52 -0500

    cleanup and migrate remaining 'iu' names to 'rex'
    Signed-off-by: Jinghao Jia <jinghao7@illinois.edu>

--- compiler/rustc_llvm/llvm-wrapper/PassWrapper.cpp
+++ compiler/rustc_llvm/llvm-wrapper/PassWrapper.cpp
@@ -394,9 +394,9 @@ extern "C" LLVMTargetMachineRef LLVMRustCreateTargetMachine(
     bool FunctionSections, bool DataSections, bool UniqueSectionNames,
     bool TrapUnreachable, bool Singlethread, bool VerboseAsm,
     bool EmitStackSizeSection, bool RelaxELFRelocations, bool UseInitArray,
+    bool RexEnabled,
     const char *SplitDwarfFile, const char *OutputObjFile,
     const char *DebugInfoCompression, bool UseEmulatedTls,
-    bool IUEnabled,
     const char *ArgsCstrBuff, size_t ArgsCstrBuffLen) {
 
   auto OptLevel = fromRust(RustOptLevel);
@@ -442,7 +442,7 @@ extern "C" LLVMTargetMachineRef LLVMRustCreateTargetMachine(
   Options.MCOptions.X86RelaxRelocations = RelaxELFRelocations;
   Options.UseInitArray = UseInitArray;
   Options.EmulatedTLS = UseEmulatedTls;
-  Options.IUEnabled = IUEnabled;
+  Options.RexEnabled = RexEnabled;
 
   if (TrapUnreachable) {
     // Tell LLVM to codegen `unreachable` into an explicit trap instruction.
