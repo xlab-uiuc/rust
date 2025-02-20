f81ea3e393adcdbd43c8d83933de932701efe1bc
Jinghao Jia <jinghao7@illinois.edu>
2025-02-20 12:39:02 -0600

    option/rex: follow the renaming of `parse_no_flag` to `parse_no_value`
    Signed-off-by: Jinghao Jia <jinghao7@illinois.edu>

--- compiler/rustc_session/src/options.rs
+++ compiler/rustc_session/src/options.rs
@@ -1568,7 +1568,7 @@ options! {
         "import library generation tool (ignored except when targeting windows-gnu)"),
     embed_bitcode: bool = (true, parse_bool, [TRACKED],
         "emit bitcode in rlibs (default: yes)"),
-    enable_rex: bool = (false, parse_no_flag, [TRACKED],
+    enable_rex: bool = (false, parse_no_value, [TRACKED],
         "(wip) some great switch for Rex project"),
     extra_filename: String = (String::new(), parse_string, [UNTRACKED],
         "extra data to put in each output filename"),
